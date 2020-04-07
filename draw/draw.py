fa61df489fe09b6d126ce95905679178f77f6c91
artemb <a.bobrovskij@gmail.com>
2020-04-07 21:19:12 +0100

    Draw lesson

---
+++ draw/draw.py
@@ -0,0 +1,8 @@
+from turtle import Turtle
+from draw.drawlib import set_up_axis
+
+s = set_up_axis()
+
+t = Turtle()
+
+s.mainloop()
---
+++ draw/drawlib.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+
+def set_up_axis():
+    s = Screen()
+    s.setup(700, 700)
+    axis_drawer = Turtle()
+    axis_drawer.color('gray')
+    axis_drawer.speed('fastest')
+    axis_drawer.penup()
+    axis_drawer.goto(- s.window_width() / 2 + 30, 0)
+    axis_drawer.pendown()
+    axis_drawer.goto(s.window_width() / 2 - 30, 0)
+    axis_drawer.stamp()
+    axis_drawer.penup()
+    axis_drawer.goto(0, - s.window_height() / 2 + 30)
+    axis_drawer.setheading(90)
+    axis_drawer.pendown()
+    axis_drawer.goto(0, s.window_height() / 2 - 30)
+    axis_drawer.stamp()
+    axis_drawer.penup()
+    axis_drawer.hideturtle()
+    axis_drawer.goto(-s.window_width() / 2 + 30, 5)
+    axis_drawer.write(f"-{int(s.window_width() / 2)}")
+    axis_drawer.goto(s.window_width() / 2 - 50, 5)
+    axis_drawer.write(f"+{int(s.window_width() / 2)}", )
+    axis_drawer.goto(5, -s.window_height() / 2 + 30)
+    axis_drawer.write(f"-{int(s.window_height() / 2)}")
+    axis_drawer.goto(5, s.window_height() / 2 - 30)
+    axis_drawer.write(f"+{int(s.window_height() / 2)}")
+
+    return s
