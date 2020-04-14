3ec7ee041d0b963358fdefe632ac063739a4860b
artemb <a.bobrovskij@gmail.com>
2020-04-14 19:52:33 +0100

    English lesson files

---
+++ console_program/calculator_en.py
@@ -0,0 +1,6 @@
+# Requirements:
+#   Accept 2 numbers and an operator from the user
+#   The operator must be one of the following: +, -, *, /
+#   Depending on the operator, the program must add, subtract, multiply or divide the 2 numbers
+#   Print the result to the console
+#   If the user types an unknown operator, print a message about it and exit
---
+++ console_program/greeter_en.py
@@ -0,0 +1,4 @@
+# Step 1. Print a welcome message to the console: "Hello, user! =) "
+# Step 2. Print "5 times 5 is 25". Use 5*5 in place of 25
+# Step 3. Ask for the user's name and greet them by name
+
