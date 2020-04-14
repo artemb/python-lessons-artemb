a67c4fb98c54e68d66333a054b06aa396f72575f
artemb <a.bobrovskij@gmail.com>
2020-04-14 20:02:07 +0100

    Grouper task

---
+++ console_program/grouper_en.py
@@ -0,0 +1,14 @@
+# The program is designed for users, who accept new students to a school.
+# There are 4 PE groups in the school:
+#    Football for boys
+#    Football for girls
+#    Athletic for boys
+#    Athletic for girls
+
+#   Requirements:
+# The user inputs the student's name
+# The user inputs the student's sex - M / F
+# The user answers a question "Does the student like football"? Yes / No
+# The program outputs the result - the name of the group for the student
+# Extra task: What if the user inputs Male instead of M? Or boy / girl?
+
