5347de168f9ca8e4805685183c4085b9f8bd11ce
artemb <a.bobrovskij@gmail.com>
2020-04-20 17:18:29 +0100

    Number guess game

---
+++ console_program/number_guess_en.py
@@ -0,0 +1,10 @@
+# Objective - create a number guessing game
+#
+# Requirements:
+#   The program chooses a random number between 1 and 100
+#   The user tries a number
+#   If the number is incorrect, the program gives a hint -
+#       if the user's number is greater or smaller than the correct number.
+#   If the number is correct, the program tells the user how many steps
+#       they used to guess the number.
+
