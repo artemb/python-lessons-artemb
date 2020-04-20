8226724bf0b448b8ff32fd08d9cf9eeafd8e2524
artemb <a.bobrovskij@gmail.com>
2020-04-20 17:12:45 +0100

    Sequence task

---
+++ console_program/sequence_sum_en.py
@@ -0,0 +1,8 @@
+# Objective - develop a program that calculate a sum of a sequence of numbers
+#
+# Requirements
+#   The program asks for a number
+#   The user enters a number
+#   The steps above are repeated until the user presses Enter without typing a number
+#   The program outputs the sum of all entered numbers
+
