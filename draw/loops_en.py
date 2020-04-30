f063ec1a2110f4b44f25fee444b2ec4702172e09
artemb <a.bobrovskij@gmail.com>
2020-04-30 18:04:49 +0100

    English loops

--- draw/loops_en.py
+++ draw/loops_en.py
@@ -1,12 +1,8 @@
-# Следующие 2 строчки создают экран с осями
 from draw.drawlib import set_up, new_shape
 screen = set_up(axis=True)
 
-# Создаем черепашку в заданном месте заданного цвета
-# Черепашка не выглядит как черепашка, она выглядит как стрелочка
 turtle = new_shape(100, 100, 'blue')
 
-# Двигаем черепашку, она оставляет за собой линию
 turtle.forward(50)
 turtle.left(90)
 turtle.forward(50)
@@ -16,20 +12,15 @@ turtle.left(90)
 turtle.forward(50)
 turtle.left(90)
 
-# Не завершаем программу, зависаем на следующей строке, пока пользователь не закроет окно
 screen.mainloop()
 
 
-# Задача 1. Черепашка уже рисует квадрат, но строки кода повторяются
-#           Используя цикл for x in range(N) уберите повторения
+# Task 1.   The turtle draws a square, but the code repeats itself
+#           Using the for in range(N) loop, remove code repetition
 #
-# Задача 2. Вместо прямоугольник нарисуйте треугольник, пятиугольник, шестиугольник
+# Task 2.   Try and draw a triangle. A hexagon. An octagon
 #
-# Задача 3. Рисование правильных многоугольников происходит одинаково,
-#           меняются лишь некоторые параметры, в зависимости от количества углов
-#           Введите переменную vex, задающую количество углов.
-#           Исправьте алгоритм таким образом, чтобы меняя значение vex можно было бы
-#           нарисовать многоугольник с любым количеством углов от 3х до 20
-#
-# Задача 4. Многоугольник с 20 вершинами уже очень похож на круг, не так ли?
-#           Нарисуйте круг.
+# Task 3.   Drawing regular polygons looks very similar.
+#           The only thing that is changing is the number of edges.
+#           Create a variable to store the number of edges.
+#           Change the code to draw any number of edges.
